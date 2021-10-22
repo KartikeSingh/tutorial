e51699525d8947246cde7efd855fede062821315
null <60427892+vierofernando@users.noreply.github.com>
2021-10-22 11:23:13 +0700

    feat(class.js): add class inheritance

--- Javascript/basic/class.js
+++ Javascript/basic/class.js
@@ -15,4 +15,18 @@ class aClass {
 
 const aObject = new aClass(59);
 
-aObject.log("Hello there youtube & a is " +aObject.a)
+aObject.log("Hello there youtube & a is " + aObject.a);
+
+// inheriting a class by 'extending it'
+
+class bClass extends aClass {
+    constructor(a, b) {
+        // call aClass's constructor
+        super(a);
+        this.b = b;
+    }
+}
+
+const bObject = new bClass(69, 420);
+console.log(bObject.a); // 69
+console.log(bObject.b); // 420
