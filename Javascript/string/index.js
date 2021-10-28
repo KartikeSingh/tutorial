da3bb119b4f8635edf22af3d4cbd06236b6f1e0f
KrazyDev <67275060+KartikeSingh@users.noreply.github.com>
2021-10-28 11:26:58 +0530

    Episode 3 | String 🧵

---
+++ Javascript/string/index.js
@@ -0,0 +1,47 @@
+const a = "   H eLlo   ", b = new String("world");
+
+// console.log(a.length);
+
+// a.charAt
+// console.log(a.charCodeAt(1));
+
+// console.log(a.concat(" ", b, b));
+
+// console.log(b.endsWith("o"));
+
+// if (a.includes("ll")) {
+//     console.log(a+" have ll")
+// }
+
+// console.log(a.indexOf("l"), a.lastIndexOf("l"));
+
+// console.log(a.match(/h/));
+
+// console.log([...a.matchAll(/h/g)]);
+
+// console.log(a.repeat(4));
+
+// console.log(a.replace(/l/gi,"z"));
+
+// console.log(a.slice(1, 3));
+
+// console.log(a.split(/ +/));
+
+// console.log(a.startsWith("H"), b.startsWith("h"));
+
+// console.log(a.substr(1, 2));
+// console.log(a.substring(1, -3));
+
+// console.log(a.toLowerCase(), a.toUpperCase());
+
+// console.log(a + "|");
+// console.log(a.trim()+"|");
+
+// console.log(a.trimLeft()+"|");
+// console.log(a.trimStart()+"|");
+
+// console.log(a.trimRight() + "|");
+// console.log(a.trimEnd() + "|");
+
+
+// console.log(a.valueOf(), a.toString());
